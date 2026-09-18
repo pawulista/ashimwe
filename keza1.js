971b0575d76a045c081f13f85bb951d93e7f2143
pawulista <jeankarekezi1010@gmail.com>
2026-09-18 16:45:41 +0200

    hc sollutions

--- keza1.js
+++ keza1.js
@@ -1,4 +1,4 @@
 console.log("how are you today");
-console.log("evening class has started");
+console.log("evening has started");
 console.log("today is the last day ");
 console.log("keep earning please");
